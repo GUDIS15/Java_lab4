a72f5560a147dd52a56ad51d43a7c11f5807c47e
GUDIS15 <91594332+GUDIS15@users.noreply.github.com>
2021-09-29 13:41:11 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,17 @@
+package LAB4;
+
+public class Q1 {
+	public static void main(String[] args) {
+		double  a= -1.0, b = 4.5;
+		double c= -5.3;
+		 	
+
+		if(a>=b && a>=c) 
+			System.out.println("The Largest number is: "+a);
+		else if (b>=a && b>=c)
+			System.out.println("The Largest number is: "+b);
+		else
+			System.out.println("The Largest number is: "+c);
+}
+
+}
---
+++ Q2.java
@@ -0,0 +1,17 @@
+package LAB4;
+
+public class Q2 {
+	public static void main(String[] args) {
+		char cha='s';
+		switch (cha) {
+		case 'a':
+		case 'b':
+		case 'i':
+			System.out.println("Vowel is:"+cha);
+			break;
+			default:
+				System.out.println("Consonant is:"+cha);
+		}
+	}
+
+}
